03c31d30a92cd2b81754599c489e18d5720a10c8
yTomi1337 <laniocha123@gmail.com>
2024-12-06 13:26:29 +0100

    frontend files added to repository

---
+++ frontend/app/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+
+
+ 
+  <p class="read-the-docs">This is a full stack project build with the help of vue.js and node.js</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
